cae902aca053558f6bc214540da2e6c2ef4beea1
yazsaazar <yazminzarate2402@gmail.com>
2023-11-16 09:20:31 -0600

    primer commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,11 +6,19 @@ import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
+import { taskRepository } from './core/tasks/interfaces/task.repository';
+import { TaskStorageService } from './infrastructure/task/TaskStorage.service';
+import {HTTP_INTERCEPTORS, HttpClientModule} from '@angular/common/http';
+import { TaskTrelloService } from './infrastructure/task/task-trello.service';
+import { TokenInterceptorService } from './infrastructure/token-interceptor.service';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
-  providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
+  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule, HttpClientModule,],
+  providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy },
+              { provide : taskRepository, useClass: TaskTrelloService},
+              {provide : HTTP_INTERCEPTORS, useClass: TokenInterceptorService, multi: true},
+            ],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
